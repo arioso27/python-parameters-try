2f7e7ab66c097a2f3d8e7be326642deaf0944bb3
arioso27 <arioso27@gmail.com>
2016-09-06 05:58:34 +0800

    debug env variables command url path

--- foobar2000-http-control-AddPlaylist/foobar2000-http-control-AddPlaylist/foobar2000_http_control_AddPlaylist.py
+++ foobar2000-http-control-AddPlaylist/foobar2000-http-control-AddPlaylist/foobar2000_http_control_AddPlaylist.py
@@ -13,12 +13,16 @@ import urllib.request
 def main( argv=None ):
 	sysCode = sys.getdefaultencoding()
 	print( "defaultencoding=", sysCode )
-	if( 1 ):
+	vsDEBUG = os.environ["VS_DEBUG"]
+	print( "vsDEBUG:", vsDEBUG )
+	print( "PYTHONDEBUG=", os.getenv( "PYTHONDEBUG" ) )
+	if( (vsDEBUG != "TRUE") and (1) ):
 		##vs2015 debug will raise exception in sys.stdout.detach(), but just run it not
 		try:
 			sys.stdout = codecs.getwriter("utf_8")(sys.stdout.detach()) ##important
+			##sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf_8')
 		except:
-			print("except error stdout")
+			print("except error stdout")		
 		try:
 			sys.stdin = codecs.getwriter("utf_8")(sys.stdin.detach()) ##important
 		except:
@@ -35,12 +39,24 @@ def main( argv=None ):
 	noArgv = len(argv)
 	if noArgv <= 1:
 		print( "No path string input ..!" )
-		exit(0)
+		return( -1 )
 	#_ if
 	print( "noArgv=", noArgv )
 	dirIN = argv[1]
 	print( "dirIN=", dirIN )
-	sys.stdout.flush()
+	dirIN = dirIN.strip('"')
+	print( "strip dirIN="+ dirIN )
+	out.flush()
+
+	servName = "192.168.1.2:8888"
+	##create playlist
+	newPlName = os.path.basename(dirIN) ## 傳回路徑的最後一個部分
+	print( "newPlName=[", newPlName, "]" )
+	#targetDir = os.path.dirname(dirIN) ## 傳回檔案名稱中屬於路徑的部分
+	paraCmd = { "cmd":"CreatePlaylist", "param1":newPlName, "param2":"0", "param3":"NoResponse" }
+	paraCmdee = urllib.parse.urlencode(paraCmd)
+	urlPath = "/ajquery/?" + paraCmdee
+	print( "urlPath=["+ urlPath +"]" )
 
 #_ def main
 
